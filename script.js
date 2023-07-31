f4906c417b0eba43ebb243b4c2a1abc4b9fe70cd
Abitoftrolling <emmanuelo@seoscholars.org>
2023-07-31 10:29:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Add your code to this file
+
+
+document.body.style["background-color"] = "rgb(0, 0, 255)";
+document.querySelector("#starters_image_div img").style.border = "dotted";
+
+let pokemon_images = document.querySelectorAll;
+
+for(let i = 0; i < pokemon_images.length; i++){
+    document.querySelectorAll(".pokemon_images");
+}
